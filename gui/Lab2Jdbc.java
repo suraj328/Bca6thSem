54b876b1b1fa4f4cbfbedf5bf560659fa26e39d5
suraj328 <shahsuraj328@gmail.com>
2023-08-13 21:38:55 +0545

    lib mysql j connector added

--- gui/Lab2Jdbc.java
+++ gui/Lab2Jdbc.java
@@ -8,7 +8,7 @@ public class Lab2Jdbc {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uthinkcrazy", "root", "uthinkcrazy");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uthinkcrazy", "root", "Suraj@984455");
 
             Statement stmt = conn.createStatement();
             ResultSet rs =  stmt.executeQuery("SELECT * FROM courses");
@@ -16,9 +16,9 @@ public class Lab2Jdbc {
             while (rs.next()) {
                 System.out.println(rs.getString(2));
             }
-
             
         } catch (Exception e) {
+            e.printStackTrace();
             System.out.println("error :-" +  e);
         }
     }
